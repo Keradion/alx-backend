ae0e562845169e18ce970aafb5365fbfe89ffaf3
Daniel <dan@gmail.com>
2024-11-05 11:06:38 +0000

    Babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,14 +5,15 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+
 class Config:
     """ Set default value for babel config variables """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-app.config.from_object(Config)
 
+app.config.from_object(Config)
 
 
 @app.route('/')
