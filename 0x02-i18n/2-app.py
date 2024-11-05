514644f4d076d1446fb6973ce100281f7b49226a
Daniel <dan@gmail.com>
2024-11-05 19:24:02 +0000

    get locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Basic flask app setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Set default value for babel config variables """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match from our supported languages."""
+    return request.accept_langauge.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """ Render a template """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
