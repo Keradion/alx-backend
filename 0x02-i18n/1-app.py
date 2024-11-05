56dd13aa760ca3eebe52e78f7c846a4fe61ea896
Daniel <dan@gmail.com>
2024-11-05 11:04:28 +0000

    Babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Basic flask app setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+class Config:
+    """ Set default value for babel config variables """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+
+
+@app.route('/')
+def index():
+    """ Render a template """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
