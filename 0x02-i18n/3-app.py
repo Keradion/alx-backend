68e871125b48fe3d8079113ea886924c876e474c
Daniel <danielshitaye10@gmail.com>
2024-11-09 00:22:45 +0300

    force locale

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,13 +20,17 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """Determine the best match from our supported languages."""
+    # Fetch locale from url parameter
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     """ Render a template that display a text"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
